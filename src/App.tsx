6aefcc46e71254e177d62ce97a155943077d6b0d
Brian Whelan <brian@friday.ie>
2022-10-27 12:33:25 +0100

    add homepage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import GlobalStyles from "./styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import myTheme from "./styles/DefaultTheme";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages";
 
 function App() {
   return (
     <ThemeProvider theme={myTheme}>
       <GlobalStyles />
-      <div className="App">
-        <h1>App</h1>
-      </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </ThemeProvider>
   );
 }
