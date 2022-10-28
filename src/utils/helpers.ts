827be078ff0fbc13c3ed09df8299b5f4ba664fd7
Brian Whelan <brian@friday.ie>
2022-10-28 13:53:19 +0100

    add debounce function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -7,3 +7,17 @@
 export const randomNumber = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+/**
+ * Debounce
+ */
+
+export const debounce = (func: (args: any) => void, delay: number) => {
+  let timerId: ReturnType<typeof setTimeout>;
+
+  return (...args: any) => {
+    const boundFunc = func.bind(this, ...args);
+    clearTimeout(timerId);
+    timerId = setTimeout(boundFunc, delay);
+  };
+};
