0f8715601efeb94e7447e9036a53f264f4107339
Brian Whelan <brian@friday.ie>
2022-10-28 13:55:29 +0100

    add new components exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { default as Loading } from "./Loading/Loading";
 export { default as MediaInfo } from "./MediaInfo/MediaInfo";
 export { default as GenresList } from "./GenresList/GenresList";
 export { default as Button } from "./Button/Button";
+export { default as Nav } from "./Nav/Nav";
+export { default as Row } from "./Row/Row";
+export { default as Slider } from "./Slider/Slider";
