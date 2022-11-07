ba7c35b89ec57637bfa41fcaebe3a7f767c552a8
Brian Whelan <brian@friday.ie>
2022-11-07 14:40:02 +0000

    edit position and paddings

--- src/components/TextHeader/TextHeader.styles.ts
+++ src/components/TextHeader/TextHeader.styles.ts
@@ -3,19 +3,25 @@ import { device } from "../../styles/breakpoints";
 import { DefaultStyleProps } from "../../types/types";
 
 export const TextHeaderWrapper = styled.section<DefaultStyleProps>`
-  position: sticky;
-  top: 0;
   background: ${(props) => props.theme.colors.background};
 `;
 
 export const TextHeaderHeading = styled.h1<DefaultStyleProps>`
-  padding: 120px 7.5% 10% 7.5%;
+  padding: 80px 7.5% 20px 7.5%;
   text-align: center;
-  font-size: 28px;
+  font-size: 22px;
   letter-spacing: 10px;
   font-weight: ${(props) => props.theme.fontWeights.bold};
-  @media (${device.md}) {
-    padding: 140px 7.5% 10% 7.5%;
+  height: 180px;
+  /* position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 12; */
+  background-color: black;
+
+  @media (${device.sm}) {
+    padding: 140px 7.5% 120px 7.5%;
     font-size: 38px;
   }
 `;
